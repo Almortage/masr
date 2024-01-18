15e0d06cd021f6140da1d229db5fe7d0ebddbc41
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-01-19 01:36:33 +0200

    Update __main__.py

--- AnonX/__main__.py
+++ AnonX/__main__.py
@@ -74,5 +74,5 @@ async def init():
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(init())
+    loop.run_until_complete(init())
     LOGGER("AnonX").info("Stopping Music Bot...")
