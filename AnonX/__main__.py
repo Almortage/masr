a97f54de6e02347e7520eaf445600a4f6b3d230b
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-01-18 23:38:35 +0200

    Add files via upload

--- AnonX/__main__.py
+++ AnonX/__main__.py
@@ -10,8 +10,8 @@ from config import BANNED_USERS
 from AnonX import LOGGER, app, userbot
 from AnonX.core.call import Anon
 from AnonX.plugins import ALL_MODULES
-from AnonX.utils.database import get_banned_users, get_gbanned
 from AnonX.plugins.azan import azkar
+from AnonX.utils.database import get_banned_users, get_gbanned
 
 loop = asyncio.get_event_loop()
 
